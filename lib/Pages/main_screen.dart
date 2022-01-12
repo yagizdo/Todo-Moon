405e7c039c64746a605e9d47e519472df8527b78
Yılmaz Yağız <dokumaciyagiz@gmail.com>
2022-01-12 17:29:46 +0300

    Added MainScreen, change home : path

---
+++ lib/Pages/main_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/Pages/profile_page.dart';
+import 'package:todo_app/provider/todos_provider.dart';
+
+import 'add_todo.dart';
+import 'calendar_page.dart';
+import 'dashboard_page.dart';
+import 'home_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //Variables
+  int currentTab = 0;
+  var pageList = [
+    const HomePage(),
+    const CalendarPage(),
+    const AddTodo(),
+    const DashboardPage(),
+    const ProfilePage()
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<TodosProvider>(context, listen: false).initSharedPreferences();
+  }
+
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = const HomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor('#f3f0e5'),
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: HexColor('#000000'),
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            currentScreen = const AddTodo();
+            currentTab = 5;
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5,
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = const HomePage();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.home,
+                          color: currentTab == 0 ? Colors.black : Colors.grey,
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35.0),
+                    child: MaterialButton(
+                      minWidth: 40,
+                      onPressed: () {
+                        setState(() {
+                          currentScreen = const CalendarPage();
+                          currentTab = 1;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.calendar,
+                            color: currentTab == 1 ? Colors.black : Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // Right Tab Icons
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = const DashboardPage();
+                        currentTab = 3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.clipboardCheck,
+                          color: currentTab == 3 ? Colors.black : Colors.grey,
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35.0),
+                    child: MaterialButton(
+                      minWidth: 40,
+                      onPressed: () {
+                        setState(() {
+                          currentScreen = const ProfilePage();
+                          currentTab = 4;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.user,
+                            color: currentTab == 4 ? Colors.black : Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ import 'package:todo_app/Pages/home_page.dart';
 import 'package:todo_app/Pages/profile_page.dart';
 import 'package:todo_app/provider/todos_provider.dart';
 
+import 'Pages/main_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -27,160 +29,10 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(title: 'ToDo App'),
+        home: const MainScreen(title: 'ToDo App'),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  //Variables
-  int currentTab = 0;
-  var pageList = [
-    const HomePage(),
-    const CalendarPage(),
-    const AddTodo(),
-    const DashboardPage(),
-    const ProfilePage()
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<TodosProvider>(context, listen: false).initSharedPreferences();
-  }
-
-  final PageStorageBucket bucket = PageStorageBucket();
-  Widget currentScreen = const HomePage();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: HexColor('#f3f0e5'),
-      body: PageStorage(
-        child: currentScreen,
-        bucket: bucket,
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: HexColor('#000000'),
-        child: const Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            currentScreen = const AddTodo();
-            currentTab = 5;
-          });
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 5,
-        child: SizedBox(
-          height: 60,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = const HomePage();
-                        currentTab = 0;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.home,
-                          color: currentTab == 0 ? Colors.black : Colors.grey,
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 35.0),
-                    child: MaterialButton(
-                      minWidth: 40,
-                      onPressed: () {
-                        setState(() {
-                          currentScreen = const CalendarPage();
-                          currentTab = 1;
-                        });
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FaIcon(
-                            FontAwesomeIcons.calendar,
-                            color: currentTab == 1 ? Colors.black : Colors.grey,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              // Right Tab Icons
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = const DashboardPage();
-                        currentTab = 3;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.clipboardCheck,
-                          color: currentTab == 3 ? Colors.black : Colors.grey,
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 35.0),
-                    child: MaterialButton(
-                      minWidth: 40,
-                      onPressed: () {
-                        setState(() {
-                          currentScreen = const ProfilePage();
-                          currentTab = 4;
-                        });
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FaIcon(
-                            FontAwesomeIcons.user,
-                            color: currentTab == 4 ? Colors.black : Colors.grey,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
