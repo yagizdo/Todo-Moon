386742acb5a7deacbd95c3e3507fff855399d958
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-03-15 18:37:14 +0300

    Update profile text

--- lib/Widgets/Profile/name_text.dart
+++ lib/Widgets/Profile/name_text.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/provider/todos_provider.dart';
@@ -10,10 +11,11 @@ class NameText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20.0),
+      padding: const EdgeInsets.only(top: 20.0, left: 10, right: 10),
       child: Consumer<TodosProvider>(
-        builder: (context, state, child) => Text(
+        builder: (context, state, child) => AutoSizeText(
           '${state.name.toTitleCase()} ${state.surname.toTitleCase()}',
+          maxLines: 1,
           style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
         ),
       ),
