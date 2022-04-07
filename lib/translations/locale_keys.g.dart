8c3ee5bece94f25aecfd9fccc07d6acece5c53da
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-07 23:57:39 +0300

    added locale keys

---
+++ lib/translations/locale_keys.g.dart
@@ -0,0 +1,14 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const hellotext = 'hellotext';
+  static const goodjobtext = 'goodjobtext';
+  static const taskstitle = 'taskstitle';
+  static const taskcount = 'taskcount';
+  static const lifebalancedtext = 'lifebalancedtext';
+  static const dasboard_tasks = 'dasboard.tasks';
+  static const dasboard_subtitle = 'dasboard.subtitle';
+  static const dasboard = 'dasboard';
+  static const homesubtitle = 'homesubtitle';
+
+}
